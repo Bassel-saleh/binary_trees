223a57002b576d5015c10caa124dca241c2045dc
Bassel-saleh <basselh26@gmail.com>
2024-02-27 06:50:53 +0200

    added task#0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,14 @@
+#include"binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t seed;
+
+	if (value == NULL)
+		return (NULL);
+	seed->int = value;
+	seed->parent = parent;
+	seed->right = NULL;
+	seed->left = NULL;
+	return (seed);
+}
