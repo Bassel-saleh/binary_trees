acdab333f716f0f1dbe6ec8296c8657e98a561d3
Bassel-saleh <basselh26@gmai.com>
2024-02-27 12:08:16 +0200

    added task#7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - goes through a binary tree using in-order traversal
+ * @tree: the tree to be checked
+ * @func: the function to print data of nodes
+ * Return: void
+*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && func != NULL)
+	{
+		binary_tree_preorder(tree->left, func);
+		func(tree->n);
+		binary_tree_preorder(tree->right, func);
+	}
+}
