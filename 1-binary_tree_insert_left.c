7b86b9472f51525ffea5667119519351619b0c0b
Bassel-saleh <basselh26@gmai.com>
2024-02-27 10:28:03 +0200

    debugging

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *seed = malloc(sizeof(binary_tree_t));
 
-	if (!parent)
+	if (parent = NULL)
 	{
 		free(seed);
 		return (NULL);
