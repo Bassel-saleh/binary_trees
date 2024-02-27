5ae904fc5621c2b34f17841370cc98db5ee2867a
Bassel-saleh <basselh26@gmail.com>
2024-02-27 07:42:15 +0200

    debugging

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,8 +1,13 @@
 #include"binary_trees.h"
-
+/**
+ * binary_tree_node - adds new node to a binary tree
+ * @parent: root of the node
+ * @value: the new node data
+ * Return: node or NULL on failure
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *seed = malloc(sizeof(binary_tree_t));;
+	binary_tree_t *seed = malloc(sizeof(binary_tree_t));
 
 	if (seed == NULL)
 	{
