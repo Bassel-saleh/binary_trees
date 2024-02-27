ad9d8a259741c03f5bb671d6dc1cba9084c9efa2
Bassel-saleh <basselh26@gmail.com>
2024-02-27 07:16:52 +0200

    debugging

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,8 +5,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *seed = malloc(sizeof(binary_tree_t));;
 
 	seed->n = value;
-	if (seed->n = NULL)
+	if (seed->n == NULL)
+	{
 		return (NULL);
+	}
 	seed->parent = parent;
 	seed->right = NULL;
 	seed->left = NULL;
