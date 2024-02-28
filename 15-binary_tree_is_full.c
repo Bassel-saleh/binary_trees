1b7820d39032e6947a9027eafb04b34747c99475
Bassel-saleh <basselh26@gmai.com>
2024-02-28 03:53:20 +0200

    debugging

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,4 +1,7 @@
 #include "binary_trees.h"
+
+int helper_isFull(const binary_tree_t *tree);
+
 /**
  * binary_tree_is_full - check if the tree is FULL
  * @tree: the tree to be checked
@@ -6,14 +9,25 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
+	return (helper_isFull(tree));
+}
+
+/**
+ * helper_isFull - helps binary_tree_full
+ * @tree: the tree to be checked
+ * Return: 1 if FULL or 0 if not and on FAILURE
+*/
+int helper_isFull(const binary_tree_t *tree)
+{
 	if (tree)
 	{
 		if ((tree->left != NULL && tree->right == NULL) ||
 			(tree->left == NULL && tree->right != NULL) ||
-			binary_tree_is_full(tree->left) == 0 ||
-			binary_tree_is_full(tree->right) == 0)
+			helper_isFull(tree->left) == 0 ||
+			helper_isFull(tree->right) == 0)
 			return (0);
-
 	}
 	return (1);
 }
