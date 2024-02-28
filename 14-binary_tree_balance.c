86ac79a4c33732ca13667528d2bee774815e25c8
Bassel-saleh <basselh26@gmai.com>
2024-02-28 02:35:50 +0200

    debugging

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,17 +9,24 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int n = 0;
 
 	if (tree)
-	{
-		if (tree->left)
-		{
-			n += 1;
-			n += binary_tree_balance(tree->left);
-		}
-		if (tree->right)
-		{
-			n -= 1;
-			n -= binary_tree_balance(tree->right);
-		}
-	}
+		n += (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 	return (n);
+}
+
+/**
+ * binary_tree_height - calculates the heigh of a binary tree
+ * @tree: the binary tree to be calculated
+ * Return: the height or 0 on failure
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l = 0, r = 0;
+
+	if (tree)
+	{
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
